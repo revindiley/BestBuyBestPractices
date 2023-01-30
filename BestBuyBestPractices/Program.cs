9475541013350cfaec92c25d485d393795f4f2f4
Devin Riley <devin.riley144@gmail.com>
2023-01-30 10:14:01 -0600

    finished exercise

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("Devin's New Department");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach(var department in departments)
+{
+    Console.WriteLine(department.DepartmentId);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
