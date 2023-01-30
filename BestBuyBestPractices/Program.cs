738ce26c02ac23b59c41bdc0427b89eff95a0d46
Devin Riley <devin.riley144@gmail.com>
2023-01-30 10:41:47 -0600

    finished ecercise 2

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -4,25 +4,46 @@ using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
 
-var config = new ConfigurationBuilder()
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-string connString = config.GetConnectionString("DefaultConnection");
+        string connString = config.GetConnectionString("DefaultConnection");
 
-IDbConnection conn = new MySqlConnection(connString);
+        IDbConnection conn = new MySqlConnection(connString);
+        #region Department Section 
+        //var departmentRepo = new DapperDepartmentRepository(conn);
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+        //departmentRepo.InsertDepartment("Devin's New Department");
 
-departmentRepo.InsertDepartment("Devin's New Department");
+        //var departments = departmentRepo.GetAllDepartments();
 
-var departments = departmentRepo.GetAllDepartments();
+        //foreach(var department in departments)
+        //{
+        //    Console.WriteLine(department.DepartmentId);
+        //    Console.WriteLine(department.Name);
+        //    Console.WriteLine();
+        //    Console.WriteLine();
+        //}
+        #endregion
 
-foreach(var department in departments)
-{
-    Console.WriteLine(department.DepartmentId);
-    Console.WriteLine(department.Name);
-    Console.WriteLine();
-    Console.WriteLine();
+        var productRepository = new DapperProductRepository(conn);
+        var products = productRepository.GetAllProducts();
+        foreach (var product in products)
+        {
+            Console.WriteLine(product.ProductID);
+            Console.WriteLine(product.Name);
+            Console.WriteLine(product.Price);
+            Console.WriteLine(product.CategoryID);
+            Console.WriteLine(product.OnSale);
+            Console.WriteLine(product.StockLevel);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
 }
